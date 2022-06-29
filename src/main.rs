36b27deb6e2b39bb0877c01d0815624a03f83a54
Mistyerious <dyeaaronjr@gmail.com>
2022-06-29 14:33:04 -0700

    Re-commiting original problems

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
 #[macro_use] extern crate rocket;
+
 pub mod routes;
-use crate::routes::user;
+
+use crate::routes::user::{create_user};
 
 fn main() {
     rocket::ignite()
-        .mount("/users", routes![user::create_user])
+        .mount("/users", routes![create_user])
         .launch();
 }
